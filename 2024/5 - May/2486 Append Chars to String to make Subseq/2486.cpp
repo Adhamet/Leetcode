e2755fecb755ccacc406b6016ca478b5cff91e28
Adham Essam <98621817+Adhamet@users.noreply.github.com>
2024-06-03 23:23:25 +0300

    acc

---
+++ 2024/5 - May/2486 Append Chars to String to make Subseq/2486.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int f = 0, len = 0;
+        while(f < s.length() && len < t.length()) {
+            if(s[f] == t[len]) len++;
+
+            f++;
+        }
+
+        return t.length() - len;
+    }
+};
